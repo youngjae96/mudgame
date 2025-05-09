75d7de80c333932c93ba97166c1e6022b9436622
youngjae96 <thrrltk321@gmail.com>
2025-05-09 18:22:30 +0900

    ㅇㅇ

--- backend/routes/docs.js
+++ backend/routes/docs.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const PatchNoteController = require('../controllers/PatchNoteController');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
-const PatchNoteController = require('../controllers/PatchNoteController');
 
 const options = {
   definition: {
@@ -21,12 +20,13 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(specs));
-
 module.exports = (app) => {
   app.get('/api/patchnotes', PatchNoteController.getAll);
   app.get('/api/patchnotes/:id', PatchNoteController.getOne);
   app.post('/api/patchnotes', PatchNoteController.create);
   app.put('/api/patchnotes/:id', PatchNoteController.update);
   app.delete('/api/patchnotes/:id', PatchNoteController.remove);
+
+  // Swagger 문서 라우트는 마지막에 등록
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs));
 }; 
